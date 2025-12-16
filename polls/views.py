61a02aabc1ce048f7d50e523037c120879874d2e
stanleybishesh <unikbisheshshrestha12@gmail.com>
2025-12-16 17:38:20 +0545

    add shortcuts & app name in urls for namespace

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import HttpResponse, render
+from django.shortcuts import HttpResponse, render, get_object_or_404
 from django.template import loader
 from .models import Question
+from django.http import Http404
 
 # Create your views here.
 def index(request):
@@ -11,7 +12,12 @@ def index(request):
   return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-  return HttpResponse("You're looking at question %s." % question_id)
+  # try:
+    # question = Question.objects.get(pk=question_id)
+    question = get_object_or_404(Question, pk=question_id)
+  # except Question.DoesNotExist:
+    # raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
   return HttpResponse("You're looking at the results of question %s." % question_id)
