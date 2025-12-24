ef37f7bca94616cb67b3cef16c83e4d115436811
stanleybishesh <unikbisheshshrestha12@gmail.com>
2025-12-24 23:41:42 +0545

    add fieldsets to add questions in admin page

--- polls/admin.py
+++ polls/admin.py
@@ -2,7 +2,15 @@ from django.contrib import admin
 
 from .models import Question, Choice
 
-admin.site.register(Question)
+# admin.site.register(Question)
 admin.site.register(Choice)
 
 # Register your models here.
+class QuestionAdmin(admin.ModelAdmin):
+    # fields = ["pub_date", "question_text"]
+    fieldsets = [
+        ('Write down your question', {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"]}),
+    ]
+
+admin.site.register(Question, QuestionAdmin)
