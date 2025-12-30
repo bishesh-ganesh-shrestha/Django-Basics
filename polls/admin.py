0c3a7c2d0b3650a754dab8bcb521e2ad9c9e272c
stanleybishesh <unikbisheshshrestha12@gmail.com>
2025-12-30 15:15:39 +0545

    add choices inline to question in admin page

--- polls/admin.py
+++ polls/admin.py
@@ -6,11 +6,18 @@ from .models import Question, Choice
 admin.site.register(Choice)
 
 # Register your models here.
+# StackedInline = horizontal display
+# TabularInline = vertical display
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 2
+
 class QuestionAdmin(admin.ModelAdmin):
     # fields = ["pub_date", "question_text"]
     fieldsets = [
         ('Write down your question', {"fields": ["question_text"]}),
         ("Date information", {"fields": ["pub_date"]}),
     ]
+    inlines = [ChoiceInline]
 
 admin.site.register(Question, QuestionAdmin)
